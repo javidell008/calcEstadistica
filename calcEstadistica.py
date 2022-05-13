53f7fed6dc407660a7e5a92e850b3f6c0eccf6c9
put0cordob3 <69874382+put0cordob3@users.noreply.github.com>
2022-05-13 12:03:29 +0200

    media, moda y mediana

--- calcEstadistica.py
+++ calcEstadistica.py
@@ -1,3 +1,4 @@
+from statistics import median, mode
 print("Bienvenido a calcEstadistica \n")
 datos_estudio = []
 datos_muestra = int(input("Introduzca el primer dato de la muestra a estudiar: "))
@@ -5,4 +6,8 @@ datos_estudio.append(datos_muestra)
 while input("¿Existen mas datos a estudiar? [S/N]") == "S":
     datos_muestra = int(input("Introduzca el primer dato de la muestra a estudiar: "))
     datos_estudio.append(datos_muestra)
+datos_estudio.sort()
 media = sum(datos_estudio) / len(datos_estudio)
+print(media)
+print(median(datos_estudio))
+print(mode(datos_estudio))
