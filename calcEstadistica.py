0c79d29b2c17fdaf7277c26c3f719be5f9b855ab
put0cordob3 <69874382+put0cordob3@users.noreply.github.com>
2022-05-14 11:34:27 +0200

    fin

--- calcEstadistica.py
+++ calcEstadistica.py
@@ -1,4 +1,4 @@
-from statistics import median, mode
+from statistics import median, mode, pstdev, variance, pstdev
 print("Bienvenido a calcEstadistica \n")
 datos_estudio = [] # Inicializamos la lista que contiene los datos de la muestra.
 datos_muestra = int(input("Introduzca el primer dato de la muestra a estudiar: "))
@@ -8,6 +8,8 @@ while input("¿Existen mas datos a estudiar? [S/N]") == "S":
     datos_estudio.append(datos_muestra) # Introducimos los datos y los almacenamos en la lista previamente definida.
 datos_estudio.sort()
 media = sum(datos_estudio) / len(datos_estudio) # Calculamos la media.
-print(media)
-print(median(datos_estudio)) # Calculamos la mediana.
-print(mode(datos_estudio)) # Calculamos la moda.
+mediana = median(datos_estudio) # Calculamos la mediana.
+moda = mode(datos_estudio) # Calculamos la moda.
+varianza = variance(datos_estudio) # Calculamos la varianza.
+desviacion_tipica = pstdev(datos_estudio) # Calculamos la desviación típica.
+print("La media es {}, la mediana es {}, la moda es {}, la varianza es {} y la desviación típica es {}".format(media, mediana, moda, varianza, desviacion_tipica))
